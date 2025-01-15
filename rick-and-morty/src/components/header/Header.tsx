d52e1a6201d3af5ff091f0945c880cdd6ac34d72
Cairon29 <josewjjunco@gmail.com>
2025-01-15 00:21:33 -0500

    nit: Minimum styles changed on header and dummy background placed

--- rick-and-morty/src/components/header/Header.tsx
+++ rick-and-morty/src/components/header/Header.tsx
@@ -6,10 +6,11 @@ export const Header = () => {
         <div>
           <img src="https://i.pinimg.com/736x/88/b5/f6/88b5f6ab99941a7d071c11d27c4501c1.jpg" alt="" />
         </div>
+        <h1>The Rick and Morty Wiki</h1>
         <nav>
-            <ul> home </ul>
-            <ul> characters </ul>
-            <ul> episodes </ul>
+            <ul> Home </ul>
+            <ul> Characters </ul>
+            <ul> Episodes </ul>
             <ul> Locations</ul>
             <ul> Docs </ul>
         </nav>
