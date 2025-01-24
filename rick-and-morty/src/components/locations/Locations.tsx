53126b519018c1cb5cc96888887a8311c53c64fe
Cairon29 <josewjjunco@gmail.com>
2025-01-24 14:13:47 -0500

    feat: Locations already rendering on the Locations page

---
+++ rick-and-morty/src/components/locations/Locations.tsx
@@ -0,0 +1,12 @@
+import { Header } from '../header/Header'
+import { LocationSection } from './LocationSection'
+import './locations-styless.css'
+
+export const Locations = () => {
+  return (
+    <>
+        <Header/>
+        <LocationSection/>
+    </>
+  )
+}
