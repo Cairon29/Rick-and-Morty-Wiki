936eb0d17065fdda79ea047cb96dbb34830d4c36
Cairon29 <josewjjunco@gmail.com>
2025-01-30 17:56:11 -0500

    fix: Responsive design applied on header component

--- rick-and-morty/src/components/header/Header.tsx
+++ rick-and-morty/src/components/header/Header.tsx
@@ -10,26 +10,32 @@ import './header-styles.css'
 export const Header = () => {
   return (
     <header id="header">
-        <div>
+        <div id="logo">
           <img src="https://i.pinimg.com/736x/88/b5/f6/88b5f6ab99941a7d071c11d27c4501c1.jpg" alt="" />
         </div>
-        <h1>The Rick and Morty Wiki</h1>
-        <nav>
-            <Link to="/home">
-              <ul className="nav-button"> <RiHome9Line/> Home </ul>
-            </Link>
-            <Link to="/characters">
-              <ul className="nav-button"> <GrYoga/> Characters </ul>          
-            </Link>
-            <Link to="/episodes">
-              <ul className="nav-button"> <IoPlaySkipForwardOutline/> Episodes </ul>
-            </Link>
-            <Link to="/locations">
-              <ul className="nav-button"> <MdOutlineMyLocation/> Locations</ul>            
-            </Link>
-            <a href="https://rickandmortyapi.com/documentation" target="_blank">
-              <ul className="nav-button"> <TiDocumentText/> Docs </ul>            
-            </a>
+        <div id="title">
+          <h1>The Rick and Morty Wiki</h1>
+        </div>
+        <nav id="menu-bar">
+            <ul>
+              <li className="nav-button">
+                <Link to="/home"> <RiHome9Line/> Home </Link>
+              </li>
+              <li className="nav-button">
+                <Link to="/characters"> <GrYoga/> Characters </Link>
+              </li>
+              <li className="nav-button">
+                <Link to="/episodes"> <IoPlaySkipForwardOutline/> Episodes </Link>
+              </li>
+              <li className="nav-button">
+                <Link to="/locations"> <MdOutlineMyLocation/> Locations </Link>
+              </li>
+              <li className="nav-button">
+                <a href="https://rickandmortyapi.com/documentation" target="_blank" rel="noopener noreferrer">
+                  <TiDocumentText/> Docs
+                </a>
+              </li>
+            </ul>
         </nav>
     </header>
   )
